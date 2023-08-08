a0b673f30a7aa9afd58b3e9a1f49a7d058847fc5
kekonline <kekonline@gmail.com>
2023-08-08 22:56:33 +0200

    did the bloody multiple select took me some time figure it out, didnt have time for bootstrap but i feel its not so hard to use

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -32,7 +32,7 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   try {
     const oneMovie = await Movie.findById(req.params.id).populate("cast");
-    console.log(oneMovie)
+    //   console.log(oneMovie)
     res.render("movies/movie-details.hbs", { oneMovie });
   } catch (error) {
     next(error);
@@ -40,60 +40,77 @@ router.get("/:id", async (req, res, next) => {
 });
 
 router.post("/:id/delete", async (req, res, next) => {
-    try {
-      await Movie.findByIdAndDelete(req.params.id)
-      res.redirect("/movies");
-    } catch (error) {
-      next(error);
-    }
-  });
-  
-  router.get("/:id/update", async (req, res, next) => {
-    
-   
-    try {
+  try {
+    await Movie.findByIdAndDelete(req.params.id)
+    res.redirect("/movies");
+  } catch (error) {
+    next(error);
+  }
+});
 
-        const responseMovieId = await Movie.findById(req.params.id)
-        const allCelebrities = await Celebrity.find().select({name: 1})
-        const cloneAllCelebrities = JSON.parse( JSON.stringify(allCelebrities))
+router.get("/:id/update", async (req, res, next) => {
 
-        cloneAllCelebrities.forEach((eachCelebrity) => {
-            if (responseMovieId.cast.toString() === eachCelebrity._id.toString()) {
-                console.log("el seleccionado es:", eachCelebrity)
-                eachCelebrity.isSelected = true;
-              }
+
+  try {
+
+    const responseMovieId = await Movie.findById(req.params.id)
+    const allCelebrities = await Celebrity.find().select({ name: 1 })
+    const cloneAllCelebrities = JSON.parse(JSON.stringify(allCelebrities))
+
+    console.log("TEST: " + responseMovieId.cast.length)
+    // if (responseMovieId.cast.length === 1) {
+
+    //   cloneAllCelebrities.forEach((eachCelebrity) => {
+    //     if (responseMovieId.cast.toString() === eachCelebrity._id.toString()) {
+    //       //  console.log("el seleccionado es:", eachCelebrity)
+
+    //       eachCelebrity.isSelected = true;
+    //     }
+    //   })
+    // } else {
+      cloneAllCelebrities.forEach((eachCelebrity) => {
+        responseMovieId.cast.forEach((eachCast) => {
+          if (eachCast.toString() === eachCelebrity._id.toString()) {
+            //  console.log("el seleccionado es:", eachCelebrity)
+            eachCelebrity.isSelected = true;
+          }
         })
-            console.log(cloneAllCelebrities)
+      })
+    // }
 
-            res.render("movies/edit-movie.hbs", {
-                oneMovie: responseMovieId,
-                allCelebrities: cloneAllCelebrities
-              })
-    } 
-    
-    catch (error) {
-      next(error);
-    }
-  });
 
-  router.post("/:id/update", (req, res, next) => {
 
-    const movieId = req.params.id; 
-    const { title, genre, plot, cast } = req.body
-  
-    Movie.findByIdAndUpdate(movieId, {  
-      title,
-      genre,
-      plot,
-      cast
+    console.log(cloneAllCelebrities)
+
+    res.render("movies/edit-movie.hbs", {
+      oneMovie: responseMovieId,
+      allCelebrities: cloneAllCelebrities
     })
+  }
+
+  catch (error) {
+    next(error);
+  }
+});
+
+router.post("/:id/update", (req, res, next) => {
+
+  const movieId = req.params.id;
+  const { title, genre, plot, cast } = req.body
+
+  Movie.findByIdAndUpdate(movieId, {
+    title,
+    genre,
+    plot,
+    cast
+  })
     .then(() => {
       res.redirect("/movies")
     })
     .catch((error) => {
       next(error)
     })
-  
-  })
+
+})
 
 module.exports = router;
